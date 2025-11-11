24e17072b43d0877fe17270bc76eeb3f0b371579
GavinKingcome <gavin@gavinkingcome.com>
2025-11-11 14:48:35 +0000

    Add School model with TDD - first passing test

--- monitoring/models.py
+++ monitoring/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class School(models.Model):
+    """Model representing a school"""
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+
 # Create your models here.
--- monitoring/tests.py
+++ monitoring/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from monitoring.models import School
 
 # Create your tests here.
+class SchoolModelTest(TestCase):
+    """Test the School model"""
+    
+    def test_create_school_with_name(self):
+        """Test that we can create a school with a name"""
+        # Create a school
+        school = School.objects.create(name="Lyndhurst Primary School")
+        
+        # Check that the school was created with the correct name
+        self.assertEqual(school.name, "Lyndhurst Primary School")
+                                       
+                                       
